d78a5c0efad193f723f5ae490b43337db18a3483
Herman <herman@slyngstadli.no>
2018-08-10 12:55:12 +0200

    add bootstrap to index.js

--- public/my-app/src/index.js
+++ public/my-app/src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import 'bootstrap/dist/css/bootstrap.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
