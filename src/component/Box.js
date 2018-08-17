2f31eb50ae221b4ed0ee3f57c8aa8f50c0197620
Herman <herman@slyngstadli.no>
2018-08-17 09:27:00 +0200

    test

---
+++ src/component/Box.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './Box.css';
+
+class Box extends Component {
+    const per = 4;
+
+    render() {
+        return (
+            <h2>Bolle</h2>
+        );
+    }
+}
