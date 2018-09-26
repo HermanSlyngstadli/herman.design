46f2f0336482d8380def2dd35a4f341d5d926638
Herman <herman@slyngstadli.no>
2018-09-26 16:04:30 +0200

    fikset ikoner til nav og margins i prosjekter

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -10,13 +10,29 @@ export class Navigation extends Component {
     }
 
     render() {
+        var linkedin = {
+            backgroundImage:"url(https://slyngstadli.no/img/linkedin.svg)"
+        }
         return(
             <nav className="main-nav">
                 <Link to='/' className="site-logo">HERMAN SLYNGSTADLI</Link>
                 <div className="nav-item-holder" id="navigation">
-                    <Link to="/work"  className="nav-item social">Work</Link>
-                    <a href="https://www.instagram.com/hermanslyngstadli/" className="nav-item social">Instagram</a>
-                    <a href="https://www.linkedin.com/in/hermanslyngstadli/" className="nav-item social">LinkedIn</a>
+                    <Link to="/work"  className="nav-item">Work</Link>
+                    <a href="https://www.linkedin.com/in/hermanslyngstadli/" className="nav-item social">
+                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 24">
+                            <path d="M19.6,19v-5.8c0-1.4-0.5-2.4-1.7-2.4c-1,0-1.5,0.7-1.8,1.3C16,12.3,16,12.6,16,13v6h-3.4c0,0,0.1-9.8,0-10.8H16v1.5c0,0,0,0,0,0h0v0C16.4,9,17.2,7.9,19,7.9c2.3,0,4,1.5,4,4.9V19H19.6z M8.9,6.7L8.9,6.7C7.7,6.7,7,5.9,7,4.9C7,3.8,7.8,3,8.9,3s1.9,0.8,1.9,1.9C10.9,5.9,10.1,6.7,8.9,6.7z M10.6,19H7.2V8.2h3.4V19z"></path>
+                        </svg>Instagram
+                    </a>
+                    <a href="https://www.instagram.com/hermanslyngstadli/" className="nav-item social">
+                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 24">
+                        <g>
+                          <path d="M15,5.4c2.1,0,2.4,0,3.2,0c0.8,0,1.2,0.2,1.5,0.3c0.4,0.1,0.6,0.3,0.9,0.6c0.3,0.3,0.5,0.5,0.6,0.9c0.1,0.3,0.2,0.7,0.3,1.5c0,0.8,0,1.1,0,3.2s0,2.4,0,3.2c0,0.8-0.2,1.2-0.3,1.5c-0.1,0.4-0.3,0.6-0.6,0.9c-0.3,0.3-0.5,0.5-0.9,0.6c-0.3,0.1-0.7,0.2-1.5,0.3c-0.8,0-1.1,0-3.2,0s-2.4,0-3.2,0c-0.8,0-1.2-0.2-1.5-0.3c-0.4-0.1-0.6-0.3-0.9-0.6c-0.3-0.3-0.5-0.5-0.6-0.9c-0.1-0.3-0.2-0.7-0.3-1.5c0-0.8,0-1.1,0-3.2s0-2.4,0-3.2c0-0.8,0.2-1.2,0.3-1.5c0.1-0.4,0.3-0.6,0.6-0.9c0.3-0.3,0.5-0.5,0.9-0.6c0.3-0.1,0.7-0.2,1.5-0.3C12.6,5.4,12.9,5.4,15,5.4M15,4c-2.2,0-2.4,0-3.3,0c-0.9,0-1.4,0.2-1.9,0.4c-0.5,0.2-1,0.5-1.4,0.9C7.9,5.8,7.6,6.2,7.4,6.8C7.2,7.3,7.1,7.9,7,8.7C7,9.6,7,9.8,7,12s0,2.4,0,3.3c0,0.9,0.2,1.4,0.4,1.9c0.2,0.5,0.5,1,0.9,1.4c0.4,0.4,0.9,0.7,1.4,0.9c0.5,0.2,1.1,0.3,1.9,0.4c0.9,0,1.1,0,3.3,0s2.4,0,3.3,0c0.9,0,1.4-0.2,1.9-0.4c0.5-0.2,1-0.5,1.4-0.9c0.4-0.4,0.7-0.9,0.9-1.4c0.2-0.5,0.3-1.1,0.4-1.9c0-0.9,0-1.1,0-3.3s0-2.4,0-3.3c0-0.9-0.2-1.4-0.4-1.9c-0.2-0.5-0.5-1-0.9-1.4c-0.4-0.4-0.9-0.7-1.4-0.9c-0.5-0.2-1.1-0.3-1.9-0.4C17.4,4,17.2,4,15,4L15,4L15,4z"></path>
+                          <path d="M15,7.9c-2.3,0-4.1,1.8-4.1,4.1s1.8,4.1,4.1,4.1s4.1-1.8,4.1-4.1S17.3,7.9,15,7.9L15,7.9z M15,14.7c-1.5,0-2.7-1.2-2.7-2.7c0-1.5,1.2-2.7,2.7-2.7s2.7,1.2,2.7,2.7C17.7,13.5,16.5,14.7,15,14.7L15,14.7z"></path>
+                          <path d="M20.2,7.7c0,0.5-0.4,1-1,1s-1-0.4-1-1s0.4-1,1-1S20.2,7.2,20.2,7.7L20.2,7.7z"></path>
+                        </g>
+                    </svg>
+                    LinkedIn
+                    </a>
                 </div>
                 <button className="nav-burger" onClick={function(){
                     var element = document.getElementById("navigation");
